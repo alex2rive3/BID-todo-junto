f70d5342b64bde3922a719e47670a4cb3193eee0
alex2rive3 <alex2rive3@gmail.com>
2023-01-11 19:12:44 -0300

    Acutualizado para la tarea TODO JUNTO

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import PersonCard from './components/PersonCard'
 function App() {
   return (
     <div className="App">
-      <PersonCard name={"Jane"} LastName= {"Doe"} Age={45} hair={"Black"} />
-      <PersonCard name={"Jhon"} LastName= {"Smith"} Age={88} hair={"Brown"} />
-      <PersonCard name={"Millard"} LastName= {"Fillmore"} Age={50} hair={"Brown"} />
-      <PersonCard name={"Maria"} LastName= {"Smith"} Age={62} hair={"Brown"} />
+      <PersonCard Name={"Jane"} LastName= {"Doe"} Age={45} HairColor={"Black"} />
+
+      <PersonCard Name={"Jhon"} LastName= {"Smith"} Age={88} HairColor={"Brown"} />
+      <PersonCard Name={"Millard"} LastName= {"Fillmore"} Age={50} HairColor={"Brown"} />
+      <PersonCard Name={"Maria"} LastName= {"Smith"} Age={62} HairColor={"Brown"} />
     </div>
   );
 }
--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,14 +1,29 @@
 import './Container.css'
 import React, {Component} from 'react';
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            agePerson: this.props.Age,
+        };
+    }
+
+    birthday = () =>{
+        let agePlusOne = this.state.agePerson+1;
+        this.setState({agePerson : agePlusOne });
+    };
+
     render(){
+        const {Name, LastName, HairColor}=this.props;
         return(
             <div className="container">
-                <h1>{this.props.LastName}, {this.props.name}</h1>
-                <p>Age: {this.props.Age} </p>
-                <p>Hair Color: {this.props.hair}</p>
+                <h1>{LastName}, {Name}</h1>
+                <p>Age: {this.state.agePerson} </p>
+                <p>Hair Color: {HairColor}</p>
+                <button onClick={this.birthday}>Birthday Button for  {Name} {LastName}</button>
             </div>
         );
     }
+    
 }
 export default PersonCard;
